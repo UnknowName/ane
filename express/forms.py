89f867cfc5b45790f1f3c918a7a85ef4224160f5
chengjn <postmaster@unkowname.win>
2018-03-16 10:56:35 +0800

    优化导入减少花费时间

--- express/forms.py
+++ express/forms.py
@@ -6,7 +6,13 @@ from django.forms import widgets
 
 
 class FileForm(forms.Form):
+    TYPES = (
+      ('import', '导入数据'),
+      ('follower', '批量修改跟单人员'),
+      ('status', '批量修改状态'),
+    )
     filename = forms.FileField(label='上传')
+    op_type = forms.ChoiceField(choices=TYPES, label='操作类型')
 
 
 class LoginForm(forms.Form):
--- express/views.py
+++ express/views.py
@@ -22,57 +22,58 @@ def data_import(request):
         form = FileForm(request.POST, request.FILES)
         if form.is_valid():
             excel_file = request.FILES['filename']
+            op_type = form.cleaned_data.get('op_type')
             if not excel_file.name.endswith(('xls', 'xlsx')):
                 return HttpResponse(u'请上传EXCEL文件!')
             gener = read_excel(excel_file.read())
             next(gener)
-            for datas in gener:
-                datas_length = len(datas)
-                if datas_length == 2:
-                    num, follower_firstname = datas
-                elif datas_length == 5:
+            if op_type == 'status':
+                for datas in gener:
+                    num, status = datas
+            if op_type == 'import':
+                expresses = list()
+                count = 0
+                for datas in gener:
                     num, start_time, orig, follower_firstname, status = datas
-                else:
-                    return HttpResponse('Pleas check the file column!')
-                # Get the User Object,If not have the user,Create user and set
-                # default Password
-                try:
-                    follower = User.objects.get(first_name=follower_firstname)
-                except User.DoesNotExist:
-                    pinyin = Pinyin()
-                    username = pinyin.get_pinyin(follower_firstname.decode('utf8'), '')
-                    follower = User.objects.create(
-                        username=username, is_staff=True, is_active=True,
-                        first_name=follower_firstname
+                    try:
+                        follower = User.objects.get(first_name=follower_firstname)
+                    except User.DoesNotExist:
+                        pinyin = Pinyin()
+                        username = pinyin.get_pinyin(
+                            follower_firstname.decode('utf8'), ''
+                        )
+                        follower = User.objects.create(
+                            username=username,
+                            is_staff=True,
+                            is_active=True,
+                            first_name=follower_firstname
+                        )
+                        follower.set_password(username)
+                        perm = Group.objects.get(name=u'跟单权限')
+                        follower.groups = [perm]
+                        follower.save()
+                    expresses.append(
+                        Express(
+                            number=num,
+                            start_time=start_time,
+                            orig=orig,
+                            follower=follower,
+                            status=status
+                        )
                     )
-                    follower.set_password(username)
-                    perm = Group.objects.get(name=u'跟单权限')
-                    follower.groups = [perm]
-                    follower.save()
-                if datas_length == 2:
-                    try:
-                        express = Express.objects.get(number=num)
-                        express.follower = follower
-                        express.save()
-                    except Express.DoesNotExist:
-                        number = Express.objects.create(
-                            number=num, follower=follower
-                        )
-                    return HttpResponse('Change follower success!')
-                elif datas_length == 5:
-                    try:
-                        number = Express(
-                            number=num, orig=orig, follower=follower, status=status
-                        )
-                        number.save()
-                        number.start_time = start_time
-                        number.save()
-                    except Exception as e:
-                        if 'Duplicate entry' in e:
-                            continue
-                        print e
-            process_time = str(time.clock()).encode('utf8')
-            return HttpResponse(u'导入数据成功，共花费时间' + process_time + u'秒')
+                    count += 1
+                    if count == 500:
+                        count = 0
+                        Express.objects.bulk_create(expresses)
+                        expresses = list()
+                        continue
+                Express.objects.bulk_create(expresses)
+                process_time = str(time.clock()).encode('utf8')
+                return HttpResponse(u'导入数据成功，共花费时间' + process_time + u'秒')
+            if op_type == 'follower':
+                for datas in gener:
+                    num, follower_firstname = datas
+                    pass
     else:
         form = FileForm()
     return render(request, 'import.html', locals())
