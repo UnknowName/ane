7bf0d68e6c18bf39e4ad9631f6c18b2f93bb332e
chengjn <postmaster@unkowname.win>
2018-03-16 13:11:51 +0800

    历史运单导出功能CSV

--- express/admin.py
+++ express/admin.py
@@ -143,43 +143,14 @@ class ExpressArchiveAdmin(admin.ModelAdmin):
         return reads
 
     def export_data(self, request, queryset):
-        export_file = 'data.xls'
-        excel = xlwt.Workbook(encoding='utf8')
-        excel_sheet = excel.add_sheet('shet1')
-        # Add EXCEL Header
-        excel_sheet.write(0, 0, '运单号')
-        excel_sheet.write(0, 1, '发件网点')
-        excel_sheet.write(0, 2, '开单时间')
-        excel_sheet.write(0, 3, '状态')
-        excel_sheet.write(0, 4, '跟进人')
-        excel_sheet.write(0, 5, '客户诉求')
-        excel_sheet.write(0, 6, '完结时间')
-        excel_sheet.write(0, 7, '完结后留言')
-        # Write Data to EXCEL
-        row = 1
-        for data in queryset:
-            datas = map(
-                to_unicode,
-                [ 
-                    str(data.number),
-                    data.orig,
-                    data.start_time.strftime('%Y-%m-%d %H:%M:%S'),
-                    data.status,
-                    data.follower.first_name,
-                    data.detail,
-                    data.end_time.strftime('%Y-%m-%d %H:%M:%S'),
-                    data.message
-                ]
-            )
-            for col, value in enumerate(datas):
-                excel_sheet.write(row, col, value)
-            row += 1
-        if os.path.exists(export_file):
-            os.remove(export_file)
-        excel.save(export_file)
-        response = StreamingHttpResponse(file_iter(export_file))
+        pseudo_buffer = Echo()
+        writer = csv.writer(pseudo_buffer)
+        response = StreamingHttpResponse(
+            (writer.writerow(datas) for datas in data_iter(queryset)),
+            content_type="text/csv"
+        )
         response['Content-Type'] = 'application/octest-stream'
-        response['Content-Disposition'] = 'attachment;filename="data.xls"' 
+        response['Content-Disposition'] = 'attachment;filename="data.csv"' 
         return response
     export_data.short_description = '导出选中数据'
 
